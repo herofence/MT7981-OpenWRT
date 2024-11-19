dab99feaca49c824ab0bed380b2962fba5139600
herofence <113574064+herofence@users.noreply.github.com>
2024-11-20 07:02:41 +0800

    Update part3.sh

--- part3.sh
+++ part3.sh
@@ -18,11 +18,11 @@ sed -i "s/pool.ntp.org/cn.pool.ntp.org/g" package/base-files/files/bin/config_ge
 echo '=========Alert NTP Server OK!========='
 
 echo '修改闭源驱动2G wifi名称'
-sed -i 's/ssid="ImmortalWrt-2.4G"/ssid="NX30PRO-2.4G"/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-sed -i 's/ssid="OpenWRT-2.4G/ssid="NX30PRO-2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/wifi/mtk.sh
+sed -i 's/ssid="ImmortalWrt-2.4G"/ssid="herofence-2.4G"/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ssid="OpenWRT-2.4G/ssid="herofence-2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/wifi/mtk.sh
 echo '=========Alert 2.4G wifi name OK!========='
 
 echo '修改闭源驱动5G wifi名称'
-sed -i 's/ssid="ImmortalWrt-5G"/ssid="NX30PRO-5G"/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-sed -i 's/ssid="OpenWRT-5G/ssid="NX30PRO-5G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/wifi/mtk.sh
+sed -i 's/ssid="ImmortalWrt-5G"/ssid="herofence-5G"/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i 's/ssid="OpenWRT-5G/ssid="herofence-5G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/wifi/mtk.sh
 echo '=========Alert 5G wifi name OK!========='
