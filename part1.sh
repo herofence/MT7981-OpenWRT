cbdbe00457a055fa00980d19d93ea2f8427716ca
herofence <113574064+herofence@users.noreply.github.com>
2024-11-20 07:00:57 +0800

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -1,3 +1,1 @@
-echo '添加Passwall依赖feeds'
-sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
-echo '=========Add passwall feeds source OK!========='
+
